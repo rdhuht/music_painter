89717c06f4fb8133f611021b4d4905024cf409c0
JeremyTsui <44908010+rdhuht@users.noreply.github.com>
2021-01-16 13:43:15 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,20 @@
+// 5353|531
+// 
+music.playMelody("G E G E G E C - ", 120)
+// 2432|5
+music.playMelody("D F E D G - - - ", 120)
+// 5353|531
+music.playMelody("G E G E G E C - ", 120)
+// 2432|1
+music.playMelody("D F E D C - - - ", 120)
+// 2244|315
+music.playMelody("D D F F E C G - ", 120)
+// 2432|5
+music.playMelody("D F E D G - - - ", 120)
+// 5353|531
+music.playMelody("G E G E G E C - ", 120)
+// 2432|1
+music.playMelody("D F E D C - - - ", 120)
+basic.forever(function () {
+	
+})
